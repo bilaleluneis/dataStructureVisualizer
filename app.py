4cbf5ac740c34a4b0e576b390f7b2ae4d1b12ec9
bilaleluneis <bilaleluneis@gmail.com>
2019-08-03 20:23:43 -0700

    update code to correct bug with memory references

--- app.py
+++ app.py
@@ -8,27 +8,12 @@ from data_structure.stacks.stack_object import Stack
 app = Flask(__name__)
 
 log.basicConfig(level=log.INFO)
-# simulate data storage that can retain values between requests
-__saved_array: ArrayListImpl = ArrayListImpl()
-__random_array: ArrayListImpl = ArrayListImpl()
-__saved_stack_list_impl: Stack = Stack(ArrayListImpl())
-__saved_stack_node_impl: Stack = Stack(ArrayNodeImpl())
 
-arrays_render_template_dic = {"array": __saved_array,
-                              "random_array": __random_array}
+arrays_render_template_dic = {"array": ArrayListImpl(),
+                              "random_array": ArrayListImpl()}
 
-stacks_render_template_dic = {"stack_list_impl": __saved_stack_list_impl,
-                              "stack_node_impl": __saved_stack_node_impl}
-
-""" 
-@app.route('/arrays')
-def load_arrays():
-    array: ArrayListImpl = ArrayListImpl()
-    for i in range(10):
-        array.insert(i)
-
-    return render_template("arrays.html", example_array=array)
-"""
+stacks_render_template_dic = {"stack_list_impl": Stack(ArrayListImpl()),
+                              "stack_node_impl": Stack(ArrayNodeImpl())}
 
 
 @app.route("/arrays", methods=["GET"])
@@ -53,20 +38,21 @@ def update_list():
         index_to_insert = int(request.form['input_index'])
 
     try:
-        __saved_array.insert(value_to_insert, index_to_insert)
+        global arrays_render_template_dic
+        arrays_render_template_dic["array"].insert(value_to_insert, index_to_insert)
         return redirect(url_for("get_arrays"))
     except ArrayIndexOutOfBoundError as insertError:
         log.error(insertError)
-        return render_template("arrays.html", array=__saved_array, error=str(insertError))
+        return render_template("arrays.html", array=arrays_render_template_dic["array"], error=str(insertError))
 
 
 def gen_random():
-    global __random_array
-    __random_array = ArrayListImpl()
+    global arrays_render_template_dic
+    arrays_render_template_dic["random_array"] = ArrayListImpl()
     size_to_generate: int = int(request.form['input_size'])
     for i in range(size_to_generate):
         int_to_insert: int = randint(0, 100)
-        __random_array.insert(int_to_insert)
+        arrays_render_template_dic["random_array"].insert(int_to_insert)
     return redirect(url_for("get_arrays"))
 
 
@@ -85,25 +71,16 @@ def post_stack():
 
 def push_to_stack():
     value_to_push: int = int(request.form['push_value'])
-    __saved_stack_list_impl.push(value_to_push)
+    global stacks_render_template_dic
+    stacks_render_template_dic["stack_list_impl"].push(value_to_push)
     return redirect(url_for("get_stacks"))
 
 
 def pop_from_stack():
-    __saved_stack_list_impl.pop()
+    global stacks_render_template_dic
+    stacks_render_template_dic["stack_list_impl"].pop()
     return redirect(url_for("get_stacks"))
 
-
-"""
-@app.route('/stacks')
-def load_stack():
-    return "stacks will go here"
-
-
-@app.route('/')
-def root():
-    return "root application"
-"""
 
 if __name__ == '__main__':
     app.run(debug=True)
